b5ea92ad3102fe7c5ba60c2b783e34b1d0ea8603
valeriane91 <jean.valerieanne@gmail.com>
2021-05-11 19:04:08 +0200

    Initialisation du projet

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <div className="board" />
+  </div>
+);
+
+// == Export
+export default App;
